dda4574bfe26f839b1d036f2c321591c0a5245e5
Jordan Brock <jordan@brock.id.au>
2018-05-02 13:47:45 +0800

    Fixed a bug in Price that failed on a nil data set

--- lib/cropster/response/price.rb
+++ lib/cropster/response/price.rb
@@ -8,12 +8,14 @@ module Cropster::Response
     attr_accessor :amount, :currency, :base_amount, :base_unit
 
     def initialize(data, base_data)
+      return if data.nil?
       load_from_data(data, base_data)
     end
 
     def load_from_data(data, base_data)
       @amount = data[:amount]
       @currency = data[:currency]
+      return if base_data.nil?
       @base_amount = base_data[:amount]
       @base_unit = base_data[:unit]
     end
